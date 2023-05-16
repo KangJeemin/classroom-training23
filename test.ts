e71442ac3212fc36c0db226d956d24f30ccc6e1c
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-05-16 23:54:33 +0900

    splice 함수 결과값이 왜?

---
+++ test.ts
@@ -0,0 +1,12 @@
+const pokemonList = ["피카츄","라이츄","파이리","꼬부기"];
+
+
+function exampleOne(array:Array<string>,change1:string,change2:string):string{
+    const result = array.splice(0,2,change1,change2)
+    console.log(result)
+    return result.join("");
+}
+
+console.log(pokemonList);
+const result =exampleOne(pokemonList,"야도란","또가스")
+console.log(result)
