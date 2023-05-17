b070eba56bde43c05990295eb45826aa609feac7
KangJeemin <wlals1234560@gmail.com>
2023-05-17 10:03:39 +0900

    splice 뒤에 문자 추가 하는 부분은 비동기인가?

--- test.ts
+++ test.ts
@@ -2,11 +2,15 @@ const pokemonList = ["피카츄","라이츄","파이리","꼬부기"];
 
 
 function exampleOne(array:Array<string>,change1:string,change2:string):string{
-    const result = array.splice(0,2,change1,change2)
+    const result = array.splice(1,2,change1,change2)
     console.log(result)
     return result.join("");
 }
 
-console.log(pokemonList);
-const result =exampleOne(pokemonList,"야도란","또가스")
-console.log(result)
+// console.log(pokemonList);
+// const result =exampleOne(pokemonList,"야도란","또가스")
+// console.log(result)
+
+
+const result2 = pokemonList.splice(0,2,"강","지")
+console.log(result2)
